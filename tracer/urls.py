64b0a8d0a8d7a2c870d0ccc809c07f626b950253
Gamaliel Petralba <petralbagamaliel@gmail.com>
2018-07-27 20:40:58 +0800

    Get coordinates from given waypoints and save to database

--- tracer/urls.py
+++ tracer/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from .views import MainTraceView
+from .views import MainTraceView, save_route
 
 app_name = 'tracer'
 
 urlpatterns = [
     path('', MainTraceView.as_view(), name="main_tracer"),
+    path('save_route/', save_route, name="save_route"),
 ]
--- tracer/views.py
+++ tracer/views.py
@@ -1,9 +1,17 @@
 from django.shortcuts import render
 from django.views import View
-
+from django.http import JsonResponse
+from .models import JeepneyRoute
 
 # Create your views here.
 class MainTraceView(View):
 
     def get(self, request):
         return render(request, 'tracer/index.html')
+
+
+def save_route(request):
+    route = JeepneyRoute(name=request.POST.get('name', None), coordinates=request.POST.get('coordinates', None))
+    route.save()
+    response = { "created": "Success" }
+    return JsonResponse(response)
