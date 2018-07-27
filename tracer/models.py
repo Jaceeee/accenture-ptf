dff91c51f83a8b43856aae0e32225e0d39fe360f
Gamaliel Petralba <petralbagamaliel@gmail.com>
2018-07-27 15:02:17 +0800

    Added Jeepney route model

--- tracer/models.py
+++ tracer/models.py
@@ -1,3 +1,5 @@
 from django.db import models
 
-# Create your models here.
+class JeepneyRoute(models.Model):
+    name = models.CharField()
+    coordinates = models.TextField(null=True, blank=True)
