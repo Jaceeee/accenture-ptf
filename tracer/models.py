11a018ffc457fb3b7dfdcaed6de52c91e50e5207
Gamaliel Petralba <petralbagamaliel@gmail.com>
2018-07-27 15:04:37 +0800

    Migrations

--- tracer/models.py
+++ tracer/models.py
@@ -1,5 +1,5 @@
 from django.db import models
 
 class JeepneyRoute(models.Model):
-    name = models.CharField()
+    name = models.CharField(max_length=255)
     coordinates = models.TextField(null=True, blank=True)
